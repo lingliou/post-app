c9fa23b8ed9a8e6d297f27cfb6fec7a005c53e83
LINGLI OU <lilyau0112@gmail.com>
2023-10-14 19:38:04 +0800

    add skeleton

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,11 +3,22 @@ const API_BASE_URL = "https://jsonplaceholder.typicode.com";
 // Function to fetch a list of posts
 export const fetchPosts = async () => {
     try {
+        // setTimeout(() => {
+        //     const response = fetch(`${API_BASE_URL}/posts`);
+        //     if (!response.ok) {
+        //         throw new Error("Network response was not ok");
+        //     }
+        //     const data = response.json();
+
+        //     return data;
+        // }, 20000);
+
         const response = await fetch(`${API_BASE_URL}/posts`);
         if (!response.ok) {
             throw new Error("Network response was not ok");
         }
         const data = await response.json();
+
         return data;
     } catch (error) {
         console.error("Error fetching posts:", error);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,18 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Pagination from "@mui/material/Pagination";
 import PostCard from "../components/PostCard";
 import { fetchPosts } from "../api/posts";
 import { useState, useEffect } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Skeleton } from "@mui/material";
 
 export default function Home() {
     const [rawPosts, setRawPosts] = useState([]);
     const [currentPosts, setCurrentPosts] = useState([]);
     const [totalPages, setTotalPages] = useState(1);
-
     const [currentPage, setCurrentPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
 
     const pageSize = 10; // Adjust as needed
 
@@ -21,15 +20,20 @@ export default function Home() {
         fetchPosts()
             .then((data) => {
                 setRawPosts(data);
-                setTotalPages(Math.ceil(data.length / pageSize));
+                if (data) {
+                    setTotalPages(Math.ceil(data.length / pageSize));
+                    setIsLoading(false);
+                }
             })
             .catch((error) => console.error("Error fetching posts:", error));
     }, []);
 
     useEffect(() => {
-        const startIndex = (currentPage - 1) * pageSize;
-        const endIndex = Math.min(startIndex + pageSize, rawPosts.length);
-        setCurrentPosts(rawPosts.slice(startIndex, endIndex));
+        if (rawPosts) {
+            const startIndex = (currentPage - 1) * pageSize;
+            const endIndex = Math.min(startIndex + pageSize, rawPosts.length);
+            setCurrentPosts(rawPosts.slice(startIndex, endIndex));
+        }
     }, [currentPage, rawPosts]);
 
     return (
@@ -38,15 +42,26 @@ export default function Home() {
                 View Posts
             </Typography> */}
             <Grid container sx={{ m: 2 }}>
-                {currentPosts.map((post) => (
-                    <Grid item xs={12} sm={6} key={post.id}>
-                        <PostCard
-                            key={post.id}
-                            title={post.title}
-                            body={post.body}
-                        />
-                    </Grid>
-                ))}
+                {isLoading
+                    ? new Array(pageSize).fill("").map((e) => (
+                          <Grid item xs={12} sm={6}>
+                              <Skeleton
+                                  variant="rounded"
+                                  //   width="50%vw"
+                                  height={110}
+                                  style={{ margin: "10px" }}
+                              />
+                          </Grid>
+                      ))
+                    : currentPosts.map((post) => (
+                          <Grid item xs={12} sm={6} key={post.id}>
+                              <PostCard
+                                  key={post.id}
+                                  title={post.title}
+                                  body={post.body}
+                              />
+                          </Grid>
+                      ))}
             </Grid>
             <Box
                 style={{
