a30c99acbd4d8c73f27baed2bf3f651613f5df6e
LINGLI OU <lilyau0112@gmail.com>
2023-10-15 14:54:07 +0800

    home post list style update

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -5,6 +5,15 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
+import { Avatar, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const CardContentNoPadding = styled(CardContent)(`
+  &:last-child {
+    padding-bottom: 0;
+  }
+`);
 
 export default function PostCard({ title, body, id }) {
     return (
@@ -22,30 +31,51 @@ export default function PostCard({ title, body, id }) {
                 },
             }}
         >
-            <CardContent>
+            <CardContentNoPadding sx={{ pb: 1 }}>
                 <Typography
                     gutterBottom
                     variant="h2"
-                    component="div"
-                    fontSize={"18px"}
-                    fontWeight={"700"}
+                    component={Link}
+                    to={`/posts/${id}`}
+                    state={{ title, body }} // Pass the post data as state
+                    sx={{
+                        textDecoration: "none",
+                        color: "inherit",
+                        fontWeight: "700",
+                        fontSize: "18px",
+                        ":hover": { textDecoration: "underline" },
+                    }}
                 >
                     {title.length > 30 ? `${title.slice(0, 30)}...` : title}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {body.length > 100 ? `${body.slice(0, 100)}...` : title}
                 </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    size="small"
-                    component={Link}
-                    to={`/posts/${id}`}
-                    state={{ title, body }} // Pass the post data as state
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        mt: 1,
+                    }}
                 >
-                    Read More
-                </Button>
-            </CardActions>
+                    <Avatar
+                        src="/broken-image.jpg"
+                        sx={{ width: 24, height: 24, my: 1 }}
+                    />
+                    <Button
+                        size="small"
+                        endIcon={<ArrowForwardIcon />}
+                        style={{ textTransform: "none" }}
+                        component={Link}
+                        to={`/posts/${id}`}
+                        state={{ title, body }} // Pass the post data as state
+                    >
+                        Read More
+                    </Button>
+                </Box>
+            </CardContentNoPadding>
         </Card>
     );
 }
